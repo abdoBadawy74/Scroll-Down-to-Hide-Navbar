e6cdd345faeec4845528d9ed8c5cb0befb423f7f
abd074 <badawyabdulrahman099@gmail.com>
2023-11-15 00:14:05 +0200

    update

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
+// add active to li when it is be clicked
 $("nav ul li").click(function () {
   $(this).addClass("active").siblings().removeClass("active");
 });
 
 let nav = document.querySelector("nav");
+
 let scrollPervious = window.pageYOffset;
+
 window.onscroll = () => {
   let scrollCurrent = window.pageYOffset;
+
   if (scrollPervious > scrollCurrent) {
     nav.style.top = "0px";
   } else {
     nav.style.top = "-90px";
   }
+
   scrollPervious = scrollCurrent;
 };
